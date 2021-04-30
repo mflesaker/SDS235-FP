f9dc3dea61a630654d2dfd2ce7fa7c029a1e3ef4
Michelle Flesaker <michelleflesaker@MacBook-Air.local>
2021-04-30 12:18:17 -0400

    changed layout

--- final_project_app/app.R
+++ final_project_app/app.R
@@ -1,11 +1,3 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
 
 library(shiny)
 library(tidyverse)
@@ -13,24 +5,35 @@ library(scales)
 
 raw_data <- read_csv("data.csv")
 
-# Define UI for application that draws a histogram
-
-
 ## code copied and modified from https://mastering-shiny.org/basic-ui.html
 ui <- fluidPage(
-    selectInput(inputId = "variable1", label = "Choose Variable 1", names(raw_data)),
-    selectInput(inputId = "variable2", label = "Choose Variable 2", names(raw_data)),
-    plotOutput("plot")
+    titlePanel("CSC/SDS 235 Final Project: Michelle, Lauryn, Grace"),
+    sidebarLayout(
+        sidebarPanel(
+            selectInput(inputId = "variable1", label = "Choose Variable 1", names(raw_data)),
+            selectInput(inputId = "variable2", label = "Choose Variable 2", names(raw_data))
+        ),
+    mainPanel(
+        plotOutput("plot")
+    )
+)
 )
 
-# Define server logic required to draw a histogram
 
-## code copied from https://mastering-shiny.org/basic-app.html
+## code copied and modified from https://mastering-shiny.org/basic-app.html and 
+# https://mastering-shiny.org/basic-ui.html
 server <- function(input, output, session) {
-    
     output$plot <- renderPlot(
         ggplot(raw_data, aes(x = get(input$variable1), y = get(input$variable2))) +
-            geom_count()
+            
+            ## geom_count idea and syntax from 
+            ## http://r-statistics.co/Top50-Ggplot2-Visualizations-MasterList-R-Code.html
+            geom_count() +
+            
+            ## adding proper x and y labels
+            ## https://web.stanford.edu/~cengel/cgi-bin/anthrospace/building-my-first-shiny-application-with-ggplot
+            xlab(input$variable1) +
+            ylab(input$variable2)
     )
 }
 
